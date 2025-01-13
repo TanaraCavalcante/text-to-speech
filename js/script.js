292de2182d0a2cbc5d72d5256573c9149bc9503e
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-13 21:22:43 +0100

    option voices in select with JavaScript

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-
+let voiceSelect = document.querySelector("select");
 let speech = new SpeechSynthesisUtterance();
 
+
+//! funzione per leggere quello che ho scritto sulla textarea
 document.querySelector("button").addEventListener("click", () =>{
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 });
+
+
+//! opzioni di diversi lingue su select
+let voices = [];
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => (voiceSelect.options[i]= new Option(voice.name,i)));
+};
