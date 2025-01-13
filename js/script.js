67bd8af4eeb3b5023fbf90470ac3cbb8ca9f5091
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-13 21:13:22 +0100

    repare speech

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ let speech = new SpeechSynthesisUtterance();
 
 document.querySelector("button").addEventListener("click", () =>{
     speech.text = document.querySelector("textarea").value;
-    window.SpeechSynthesis.speak(speech);
+    window.speechSynthesis.speak(speech);
 });
